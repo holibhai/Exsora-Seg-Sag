250fb793c1a1bbf4c8a5146fa5e201b473ffa4fe
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-11 23:47:54 +0530

    util class updated

--- src/main/java/com/crudoperation/jw/utils/Utils.java
+++ src/main/java/com/crudoperation/jw/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.crudoperation.jw.utils;
 
 import com.crudoperation.jw.dto.UserAccountDto;
+import com.crudoperation.jw.model.Role;
 import com.crudoperation.jw.model.User;
 
 public class Utils {
@@ -12,6 +13,7 @@ public class Utils {
            userAccountDto.setPassword(user.getPassword());
            userAccountDto.setFirstName(user.getFirstName());
            userAccountDto.setLastName(user.getLastName());
+           userAccountDto.setRole(user.getRole());
            return userAccountDto;
 
 
