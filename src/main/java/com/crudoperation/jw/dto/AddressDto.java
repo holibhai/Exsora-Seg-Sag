dfa262cb57cea8cd60cd4e7d3cf201f4a2fbcd4f
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-11 23:04:02 +0530

    address class created

---
+++ src/main/java/com/crudoperation/jw/dto/AddressDto.java
@@ -0,0 +1,17 @@
+package com.crudoperation.jw.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AddressDto {
+
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+    private UserAccountDto userAccount;
+    private List<UserAccountDto> userAccountDtoList;
+}
