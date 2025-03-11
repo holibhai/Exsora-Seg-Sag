0aebeda1db12995146f6fa77b1e3a279381c1e86
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-11 23:01:30 +0530

    util class created

---
+++ src/main/java/com/crudoperation/jw/utils/Utils.java
@@ -0,0 +1,19 @@
+package com.crudoperation.jw.utils;
+
+import com.crudoperation.jw.dto.UserAccountDto;
+import com.crudoperation.jw.model.User;
+
+public class Utils {
+
+    public static UserAccountDto mapUserEntityToUserDTO(User user){
+           UserAccountDto userAccountDto = new UserAccountDto();
+           userAccountDto.setId(user.getId());
+           userAccountDto.setUsername(user.getUsername());
+           userAccountDto.setPassword(user.getPassword());
+           userAccountDto.setFirstName(user.getFirstName());
+           userAccountDto.setLastName(user.getLastName());
+           return userAccountDto;
+
+
+    }
+}
