f53d24e689aa0d6615124a52a1dedf4c300bf31e
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-13 18:11:24 +0530

    productType updated

---
+++ src/main/java/com/crudoperation/jw/repo/ProductTypeRepository.java
@@ -0,0 +1,7 @@
+package com.crudoperation.jw.repo;
+
+import com.crudoperation.jw.model.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductTypeRepository extends JpaRepository<ProductType, Long> {
+}
