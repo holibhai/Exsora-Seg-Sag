7ab67385821fff1ddf2db9394e4b3897ca629daa
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-16 16:55:37 +0530

    files created

--- src/main/java/com/crudoperation/jw/controller/CatagorieController.java
+++ src/main/java/com/crudoperation/jw/controller/CatagorieController.java
@@ -1,9 +1,24 @@
 package com.crudoperation.jw.controller;
 
+import com.crudoperation.jw.dto.Response;
+import com.crudoperation.jw.model.Catagorie;
+import com.crudoperation.jw.service.serviceImp.CatagorieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/catagorie")
 public class CatagorieController {
+
+    @Autowired
+    private CatagorieService catagorieService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Response> add(@RequestBody Catagorie catagorie){
+        return ResponseEntity.ok(catagorieService.add(catagorie));
+    }
 }
--- src/main/java/com/crudoperation/jw/dto/CatagorieDto.java
+++ src/main/java/com/crudoperation/jw/dto/CatagorieDto.java
@@ -1,4 +1,50 @@
 package com.crudoperation.jw.dto;
 
 public class CatagorieDto {
+
+    private int id;
+    private String catagorieType;
+    private String catagorieId;
+    private String catagorieDescription;
+
+    public CatagorieDto(int id, String catagorieType, String catagorieId, String catagorieDescription) {
+        this.id = id;
+        this.catagorieType = catagorieType;
+        this.catagorieId = catagorieId;
+        this.catagorieDescription = catagorieDescription;
+    }
+    public CatagorieDto() {}
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCatagorieType() {
+        return catagorieType;
+    }
+
+    public void setCatagorieType(String catagorieType) {
+        this.catagorieType = catagorieType;
+    }
+
+    public String getCatagorieId() {
+        return catagorieId;
+    }
+
+    public void setCatagorieId(String catagorieId) {
+        this.catagorieId = catagorieId;
+    }
+
+    public String getCatagorieDescription() {
+        return catagorieDescription;
+    }
+
+    public void setCatagorieDescription(String catagorieDescription) {
+        this.catagorieDescription = catagorieDescription;
+    }
 }
