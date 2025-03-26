bee85ea8519eb64719b6007c32bf912ab9fba313
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-26 23:40:43 +0530

    productrepo added

--- src/main/java/com/crudoperation/jw/repo/ProductRepository.java
+++ src/main/java/com/crudoperation/jw/repo/ProductRepository.java
@@ -2,10 +2,13 @@ package com.crudoperation.jw.repo;
 
 import com.crudoperation.jw.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
-
+    @Query("SELECT p.ProductQuantity FROM Product p WHERE p.id = :id")
+    int findProductQuantityById(@Param("id") int id);
 }
