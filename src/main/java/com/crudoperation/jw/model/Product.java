1c3298b238c8a010e77022a81a665757582feacb
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-26 23:41:32 +0530

    product added

--- src/main/java/com/crudoperation/jw/model/Product.java
+++ src/main/java/com/crudoperation/jw/model/Product.java
@@ -2,6 +2,8 @@ package com.crudoperation.jw.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "product")
 public class Product {
@@ -13,30 +15,40 @@ public class Product {
     private String ProductName;
     private String ProductDescription;
     private float ProductPrice;
+    private float discount;
     private int ProductQuantity;
-    private String productStatus;
-    private boolean isAvailable;
-
-
+    private float width;
+    private float height;
+    private float depth;
+    private String warrantyInf;
+    private String Description;
+    private Date date;
+    private String productType;
+    private String category;
     private String imageName;
     private String imageType;
     @Lob
     private byte[] imageData;
 
-    private String productType;
 
-    public Product(int id, String productName, String productDescription, float productPrice, int productQuantity, String productStatus, boolean isAvailable, String imaageName, String imageType, byte[] imageData, String productType) {
+    public Product(int id, String productName, String productDescription, float productPrice, float discount, int productQuantity, float width, float height, float depth, String warrantyInf, String description, Date date, String productType, String category, String imageName, String imageType, byte[] imageData) {
         this.id = id;
         ProductName = productName;
         ProductDescription = productDescription;
         ProductPrice = productPrice;
+        this.discount = discount;
         ProductQuantity = productQuantity;
-        this.productStatus = productStatus;
-        this.isAvailable = isAvailable;
-        this.imageName = imaageName;
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.warrantyInf = warrantyInf;
+        Description = description;
+        this.date = date;
+        this.productType = productType;
+        this.category = category;
+        this.imageName = imageName;
         this.imageType = imageType;
         this.imageData = imageData;
-        this.productType = productType;
     }
 
     public Product() {
@@ -75,6 +87,14 @@ public class Product {
         ProductPrice = productPrice;
     }
 
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
     public int getProductQuantity() {
         return ProductQuantity;
     }
@@ -83,20 +103,68 @@ public class Product {
         ProductQuantity = productQuantity;
     }
 
-    public String getProductStatus() {
-        return productStatus;
+    public float getWidth() {
+        return width;
     }
 
-    public void setProductStatus(String productStatus) {
-        this.productStatus = productStatus;
+    public void setWidth(float width) {
+        this.width = width;
     }
 
-    public boolean isAvailable() {
-        return isAvailable;
+    public float getHeight() {
+        return height;
     }
 
-    public void setAvailable(boolean available) {
-        isAvailable = available;
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getDepth() {
+        return depth;
+    }
+
+    public void setDepth(float depth) {
+        this.depth = depth;
+    }
+
+    public String getWarrantyInf() {
+        return warrantyInf;
+    }
+
+    public void setWarrantyInf(String warrantyInf) {
+        this.warrantyInf = warrantyInf;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public String getImageName() {
@@ -114,7 +182,6 @@ public class Product {
     public void setImageType(String imageType) {
         this.imageType = imageType;
     }
-
     public byte[] getImageData() {
         return imageData;
     }
@@ -123,11 +190,4 @@ public class Product {
         this.imageData = imageData;
     }
 
-    public String getProductType() {
-        return productType;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
 }
