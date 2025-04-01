eb78429b0a73fa04eedd5425f97d247576cfac60
holibhai <146805057+holibhai@users.noreply.github.com>
2025-04-01 21:13:24 +0530

    cartItem controller updated

--- src/main/java/com/crudoperation/jw/controller/CartItemController.java
+++ src/main/java/com/crudoperation/jw/controller/CartItemController.java
@@ -2,12 +2,10 @@ package com.crudoperation.jw.controller;
 
 import com.crudoperation.jw.dto.Response;
 import com.crudoperation.jw.model.CartItem;
-import com.crudoperation.jw.service.CartItemService;
+import com.crudoperation.jw.service.serviceImp.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cartItem")
@@ -16,7 +14,15 @@ public class CartItemController {
     @Autowired
     private CartItemService cartItemService;
 
-    public ResponseEntity<Response> createCartItem( @RequestBody CartItem cartItem) {
-        return ResponseEntity.ok(cartItemService.createCartItem());
+    @PostMapping("/add")
+    public ResponseEntity<Response> addCartItem(@RequestParam int productId,@RequestParam int quantity,@RequestParam int userId) {
+        return ResponseEntity.ok(cartItemService.createCartItem(productId, quantity,userId));
     }
+
+    @GetMapping("/get/{userId}")
+    public ResponseEntity<Response> getItem(@PathVariable int userId) {
+        return ResponseEntity.ok(cartItemService.getItems(userId));
+    }
+
+
 }
