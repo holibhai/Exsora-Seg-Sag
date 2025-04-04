8067d15270e8a88b76cdd5c12a77df6ccc1d32a1
holibhai <146805057+holibhai@users.noreply.github.com>
2025-04-04 22:43:02 +0530

    billing model created

---
+++ src/main/java/com/crudoperation/jw/model/Billing.java
@@ -0,0 +1,84 @@
+package com.crudoperation.jw.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "billing")
+public class Billing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String mobileNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Billing(int id, String firstName, String lastName, String email, String mobileNumber, User user) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+        this.user = user;
+    }
+
+    public Billing() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
