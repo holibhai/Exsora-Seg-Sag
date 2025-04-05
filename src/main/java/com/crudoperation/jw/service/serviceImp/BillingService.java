3ab513e35511db488ad8595b2dc0a543f569880d
holibhai <146805057+holibhai@users.noreply.github.com>
2025-04-05 11:17:15 +0530

    billing service handle

--- src/main/java/com/crudoperation/jw/service/serviceImp/BillingService.java
+++ src/main/java/com/crudoperation/jw/service/serviceImp/BillingService.java
@@ -2,7 +2,9 @@ package com.crudoperation.jw.service.serviceImp;
 
 import com.crudoperation.jw.dto.Response;
 import com.crudoperation.jw.model.Billing;
+import com.crudoperation.jw.model.Delivery;
 import com.crudoperation.jw.repo.BillingRepository;
+import com.crudoperation.jw.repo.DeliveryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +14,17 @@ public class BillingService {
     @Autowired
     private BillingRepository billingRepository;
 
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
     public Response addBill(Billing billing) {
         Response response = new Response();
         try{
+            Delivery delivery = billing.getDelivery();
+            if(delivery !=null){
+                Delivery delivery1=deliveryRepository.save(delivery);
+                billing.setDelivery(delivery1);
+            }
             billingRepository.save(billing);
             response.setMessage("Successfully addeed billing");
             response.setStatusCode(201);
