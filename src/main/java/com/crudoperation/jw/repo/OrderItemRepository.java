c9348abc6d769afc060ed82adf6a13ae18de177a
holibhai <146805057+holibhai@users.noreply.github.com>
2025-04-06 14:44:45 +0530

    order item repository  created

---
+++ src/main/java/com/crudoperation/jw/repo/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.crudoperation.jw.repo;
+
+import com.crudoperation.jw.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+
+}
