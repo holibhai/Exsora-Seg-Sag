e587fdb2a533ec90b4ebec7dcdcd79d73fb6f56a
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-10 23:45:04 +0530

    Billing controller upadted

--- src/main/java/com/crudoperation/jw/controller/BillingController.java
+++ src/main/java/com/crudoperation/jw/controller/BillingController.java
@@ -5,10 +5,7 @@ import com.crudoperation.jw.model.Billing;
 import com.crudoperation.jw.service.serviceImp.BillingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/billing")
@@ -20,5 +17,8 @@ public class BillingController {
     @PostMapping("/add")
     public ResponseEntity<Response>addBilling(@RequestBody  Billing billing){
          return ResponseEntity.ok(billingService.addBill(billing));
+    }
+    public ResponseEntity<Response>deleteBilling(@PathVariable int id){
+        return ResponseEntity.ok(billingService.deleteBilling(id));
     }
 }
