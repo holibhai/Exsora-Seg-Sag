3a4956f6416681884fcf5429d35d42a0ef47cd54
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-11 02:34:27 +0530

    review service upadted

--- src/main/java/com/crudoperation/jw/service/serviceImp/ReviewService.java
+++ src/main/java/com/crudoperation/jw/service/serviceImp/ReviewService.java
@@ -3,11 +3,13 @@ package com.crudoperation.jw.service.serviceImp;
 import com.crudoperation.jw.model.Review;
 import com.crudoperation.jw.repo.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService {
@@ -26,5 +28,31 @@ public class ReviewService {
 
     public List<Review> getReviewsByUserId(Long userId) {
         return reviewRepository.findByUserId(userId);
+    }
+
+    public List<Review> getAllReviews() {
+         return reviewRepository.findAll();
+    }
+
+    public Review updateStatus(int id) {
+        Optional<Review> review = reviewRepository.findById(id);
+        if(review.isPresent()) {
+            Review review1 = review.get();
+            review1.setComment("saddi");
+            System.out.println(review1.getStatus());
+            if (review1.getStatus().equals("not post")) {
+                review1.setStatus("post");
+                return reviewRepository.save(review1);
+
+
+            }
+            if (review1.getStatus().equals("post")) {
+                review1.setStatus("not post");
+                return reviewRepository.save(review1);
+            }
+        }
+
+        return null;
+
     }
 }
